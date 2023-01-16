ac5ab85fe6f1fe64336b5607f867164cee944d29
Tony Razzor <tonyxrazzor@yandex.ru>
2023-01-16 08:00:38 +0300

    Тест проекта №8: ver 1.8

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -7,10 +7,8 @@ DEF_POST = 10
 
 def index(request):
     posts = Post.objects.all()[:DEF_POST]
-    title = 'Главная страница'
     context = {
         'posts': posts,
-        'title': title
     }
     return render(request, 'posts/index.html', context)
 
@@ -18,10 +16,8 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.select_related()[:10]
-    title = 'Страница Групп'
     context = {
         'group': group,
         'posts': posts,
-        'title': title
     }
     return render(request, 'posts/group_list.html', context)
